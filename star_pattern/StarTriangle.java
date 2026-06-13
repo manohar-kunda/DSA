ef4cf2ec4bb0348c06230fae6ed3a287f5b6fdfa
Manohar Kunda <manoharkunda5@gmail.com>
2026-06-13 21:54:10 +0530

    StarTriangle.java and .class files added

---
+++ star_pattern/StarTriangle.java
@@ -0,0 +1,23 @@
+package star_pattern;
+/*
+
+
+*
+**
+***
+****
+*****
+
+*/
+public class StarTriangle {
+    public static void main(String[] args) {
+        int n=5;
+        int i,j;
+        for(i=1;i<=n;i++) {
+            for(j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
