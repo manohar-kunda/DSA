ed1a458cc9baaa9f69fdd972153f69cf43ad0589
Manohar Kunda <manoharkunda5@gmail.com>
2026-06-13 21:26:57 +0530

    Stars_5N5.java first file added

---
+++ star_pattern/Stars_5N5.java
@@ -0,0 +1,22 @@
+package star_patterns;
+/*
+
+*****
+*****
+*****
+*****
+*****
+
+*/
+public class Stars_5N5 {
+	public static void main(String[] args) {
+		int n=5;
+		int i,j;
+		for(i=1;i<=n;i++) {
+			for(j=1;j<=n;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
