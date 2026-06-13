27809aee86830fe59094fe5a87cdf96f3b237a52
Manohar Kunda <manoharkunda5@gmail.com>
2026-06-13 21:35:13 +0530

    star_pattern/Numbers_5X5.class         star_pattern/Numbers_5X5.java files added

---
+++ star_pattern/Numbers_5X5.java
@@ -0,0 +1,14 @@
+package star_pattern;
+
+public class Numbers_5X5 {
+    public static void main(String[] args) {
+        int n=5;
+        int i,j;
+        for(i=1;i<=n;i++) {
+            for(j=1;j<=n;j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
